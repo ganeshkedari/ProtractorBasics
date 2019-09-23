5b959e3120379709ffb7a880ed59399cdf1e192b
ganeshkedari <ganeshkedari@live.com>
2019-09-23 16:01:26 +0530

    Winstone Logs

---
+++ Utilities/Winstone_logs/log.js
@@ -0,0 +1,10 @@
+﻿var winston = require('winston');
+
+
+winston.remove(winston.transports.Console);
+winston.add(winston.transports.Console, { timestamp: true });
+// winston.add(winston.transports.File, { filename: 'winston-basic.log' });
+winston.configure({transports: [new winston.transports.File({ filename: 'logfile.log' }) ]});
+module.exports = winston;
+
+ 
